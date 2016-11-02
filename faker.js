95a1698c4dbd027a2255cab956336f967f169e69
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2016-11-02 23:36:12 +0900

    Initial commit

---
+++ faker.js
@@ -0,0 +1,14 @@
+'using scrip'
+
+module.exports = function() {
+  var faker = require('faker')
+
+  return {
+    users: Array.from(Array(10), (_, i) => {
+      return {
+        id: i + 1,
+        name: faker.name.findName()
+      }
+    })
+  }
+}
